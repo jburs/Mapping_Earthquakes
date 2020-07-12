1d9b677b6aa1063f1514900806181f84901f48c2
Jason Bursey <jason.bursey1@gmail.com>
2020-07-12 19:37:20 -0400

    line mapping code

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -35,8 +35,25 @@ L.circle([34.0522, -118.2437], {
 }).addTo(map);
 
 
+// Create lines!!
+// Coordinates for each point to be used in the line.
+
+let locations = [];
+for (i=0; i<cityData.length; i++){
+	locations.push(cityData[i].location)
+};
+console.log(locations);
+
+
+// create a polyline using the line coordinates and make the line red. 
+L.polyline(locations, {
+	color: "blue",
+	opacity: .5,
+	dashArray:6,
+}).bindPopup("zoom zoom!").addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
