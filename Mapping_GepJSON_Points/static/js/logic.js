f57b0e15ef690cef614f903eba709c5c3b9f5703
Jason Bursey <jason.bursey1@gmail.com>
2020-07-13 20:02:23 -0400

    geoJSON started

--- Mapping_GepJSON_Points/static/js/logic.js
+++ Mapping_GepJSON_Points/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level (scale 0-18).
 // alternative form:  let map = L.map("mapid", {center: [40.7, -94.5],zoom: 4});
-let map = L.map('mapid').setView([40, -95], 4);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // get data from cities.js
 let cityData = cities;
@@ -35,8 +35,25 @@ L.circle([34.0522, -118.2437], {
 }).addTo(map);
 
 
+// Create lines!!
+// Coordinates for each point to be used in the line.
+
+let locations = [];
+for (i=0; i<cityData.length; i++){
+	locations.push(cityData[i].location)
+};
+console.log(locations);
+
+
+// create a polyline using the line coordinates and make the line red. 
+L.polyline(locations, {
+	color: "blue",
+	opacity: .5,
+	dashArray:6,
+}).bindPopup("zoom zoom!").addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -64,3 +81,53 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
+
+
+// Add GeoJSON data
+let sanFanAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+    "geometry":{
+        "type":"Point",
+        "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// create geoJSON layer and add to map
+// L.geoJSON(sanFanAirport).addTo(map);
+
+/*
+// pointToLayer callback function: adds markers to map  13.5.2
+L.geoJSON(sanFanAirport, {
+	// we turn each featre into a marker on the map
+	pointToLayer: function(feature, latlng) {
+		console.log(feature);
+		return L.marker(latlng)
+		.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+	}
+}).addTo(map);
+*/
+
+
+// onEachFeature callback fucntion:  can add a popup marker for each feature and add data from the properties
+L.geoJson(sanFanAirport, {
+    onEachFeature: function(feature, layer) {
+		console.log(layer)
+    	layer.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+     }
+}).addTo(map);
+
+
+
+13.5.3
